92c395d920ae4e43d8e5e9388dcf5f90be21b93f
Adrian Matos <=>
2016-06-02 23:08:42 +0800

    Add title to scroll view

--- components/Home.js
+++ components/Home.js
@@ -10,22 +10,33 @@ import {
 
 import Toolbar from './Toolbar';
 import ChapterList from './ChapterList';
+import { AdMobBanner, AdMobInterstitial } from 'react-native-admob'
+
+AdMobInterstitial.setAdUnitId('ca-app-pub-1547807753053427/3801442596');
 
 class Home extends Component {
 	render() {
 		return (
 			<View style={styles.container}>
 				<Toolbar
-					icon='menu'
+					icon='menu'	
 					title='Home' 
 					onPress={this._onIconPress.bind(this)}/>
-				<Text style={styles.title}>The Criminal Code of the Philippines</Text>
 				<ScrollView>
+					<Text style={styles.title}>The Criminal Code of the Philippines</Text>
 					<ChapterList {...this.props} />
 				</ScrollView>
+				<AdMobBanner
+				  bannerSize={"banner"}
+				  adUnitID={"ca-app-pub-1547807753053427/1406379399"}
+				  didFailToReceiveAdWithError={this.bannerError.bind(this)} />
 			</View>
 		);
 	}	
+
+	bannerError(error) {
+		ToastAndroid.show(error, ToastAndroid.SHORT);
+	}
 
 	_onIconPress() {
 		const {drawer} = this.props.route;
